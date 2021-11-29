ef105dc02fddab6376cbceff21459c2c8c0aa58f
Evans <work.evans020@gmail.com>
2021-11-29 13:36:07 +0300

    preparing frontend for heroku hosting

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static('./dist/global'))
+
+app.get('/*', (req,res) =>{
+    res.sendFile('./src/index.js', {root: "./dist/global/"})
+})
+
+app.listen(process.env.PORT || 4200, ()=>{
+    console.log('server started on port', process.env.PORT ||4200)
+})
