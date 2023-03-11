ff7af756bb83f9fc8d22391dd8f867a5bddee4bc
snaveware <work.evans020@gmail.com>
2023-03-11 15:09:24 +0300

    previous updates

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -6,60 +6,52 @@ import { UtilsService } from 'src/app/services/utils/utils.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-  errorMessage:string = 'Login error'
-  showError:boolean = false
-  isLoading:boolean = false
-  constructor(
-    private authService: AuthService,
-    private router:Router
-  ) {
-    
-   }
+  errorMessage: string = 'Login error';
+  showError: boolean = false;
+  isLoading: boolean = false;
+  constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {
-    if(UtilsService.checkInitialLoginStatus()){
-      this.router.navigate(['/'])
+    if (UtilsService.checkInitialLoginStatus()) {
+      this.router.navigate(['/']);
     }
+
+    alert(
+      'Guest Login Email: info@snaveware.com  , Guest Login Password: password'
+    );
   }
 
-  onSubmit(email,password){
+  onSubmit(email, password) {
+    this.isLoading = true;
 
-    this.isLoading = true
-    
-    this.authService.login({email,password})
-    .subscribe(result =>{
-      this.isLoading = false
-      this.afterSubscribing(result)
-    })
-    
+    this.authService.login({ email, password }).subscribe((result) => {
+      this.isLoading = false;
+      this.afterSubscribing(result);
+    });
   }
 
-  selected(selection){
-    this.showError = false
+  selected(selection) {
+    this.showError = false;
   }
 
-  afterSubscribing(result){
-    if(!result){
-      this.errorMessage = 'unknown result. Contact the admin'
-      this.showError =true
+  afterSubscribing(result) {
+    if (!result) {
+      this.errorMessage = 'unknown result. Contact the admin';
+      this.showError = true;
     }
-    if(result.isError){
-      this.errorMessage = result.message
-      this.showError =true
-      console.log('error login',result.message)
+    if (result.isError) {
+      this.errorMessage = result.message;
+      this.showError = true;
+      console.log('error login', result.message);
+    } else {
+      this.authService.isLoggedIn = { state: true, details: result };
+      this.router.navigateByUrl('/').then(() => {
+        location.reload();
+      });
     }
-    else{
-      this.authService.isLoggedIn = {state:true,details:result}
-      this.router.navigateByUrl('/')
-      .then(()=>{
-        location.reload()
-      })
-      
-    }
-    console.log('login',result)
+    console.log('login', result);
   }
-
 }
